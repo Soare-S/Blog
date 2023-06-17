56776e9a105b945943409b22551bafddb72b0959
Sorin Slesari <sorinslesari@Sorins-MacBook-Pro.local>
2023-06-17 23:57:00 +0100

    Adding static and templates folders with style and index files

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
-from flask import Flask
+from flask import Flask, request, render_template, redirect
+import json
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    with open('blog_posts.json') as json_file:
+        blog_posts = json.load(json_file)
+    return render_template('index.html', posts=blog_posts)
 
 
 if __name__ == '__main__':
